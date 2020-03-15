deb394c60c2505d6afa3ab3befdd8a500a88ee7f
unknown <C:\Users\\AppData\Roaming\The Bat!>
2020-03-15 11:26:48 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let a = String(n); 
+    let a1 = a.match(/[0-9]/g);
+    let b = a1.reverse().join("");
+    let c = Number(b);
+   
+    return(c);
 }
